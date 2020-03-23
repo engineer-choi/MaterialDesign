7800c528b65166556bf3b289d5705d8563c4ccee
CHJUNE-android <chjune0205@gmail.com>
2020-03-23 11:10:43 +0900

    TransformationLayout 이용한 intent

--- app/src/main/java/com/example/materialexercise/DetailActivity.kt
+++ app/src/main/java/com/example/materialexercise/DetailActivity.kt
@@ -1,18 +1,48 @@
 package com.example.materialexercise
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.example.materialexercise.recycler.Poster
+import com.skydoves.transformationlayout.TransformationLayout
+import com.skydoves.transformationlayout.onTransformationEndContainer
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
+    companion object{
+        const val parmasExtraName = "parmasExtraName"
+        const val posterExtraName = "posterExtraName"
+        fun startActivity(
+            context : Context,
+            transformationLayout: TransformationLayout,
+            poster : Poster
+        ) {
+            if(context is Activity){
+                val bundle = transformationLayout.withView(transformationLayout, poster.name)
+                val intent = Intent(context,DetailActivity::class.java)
+                intent.putExtra(parmasExtraName, transformationLayout.getParcelableParams())
+                intent.putExtra(posterExtraName, poster)
+                context.startActivity(intent, bundle)
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+        onTransformationEndContainer(intent.getParcelableExtra(parmasExtraName))
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         init()
     }
     private fun init(){
-        Glide.with(this).load("https://cdn.pixabay.com/photo/2018/03/07/17/15/balloon-3206530__340.jpg").into(img_detailView)
+        intent.getParcelableExtra<Poster>(posterExtraName)?.let{
+            Glide.with(this)
+                .load(it.poster)
+                .into(img_detailView)
+            tv_detail_title.text = it.name
+            tv_detail_description.text = it.description
+        }
     }
 }
--- app/src/main/java/com/example/materialexercise/recycler/PosterViewHolder.kt
+++ app/src/main/java/com/example/materialexercise/recycler/PosterViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.materialexercise.recycler
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.materialexercise.DetailActivity
 import kotlinx.android.synthetic.main.item_poster.view.*
 import kotlinx.android.synthetic.main.item_poster_menu.view.*
 import kotlinx.android.synthetic.main.item_poster_menu.view.item_poster_title
@@ -14,7 +15,7 @@ class PosterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
             item_poster_title.text = poster.name
             item_poster_running_time.text = poster.playtime
             setOnClickListener {
-
+                DetailActivity.startActivity(context,item_poster_transformationLayout,poster)
             }
         }
     }
